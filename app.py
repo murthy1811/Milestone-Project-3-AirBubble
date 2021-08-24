40400c493e128e90468ff9883c86467f49e35bf4
Murthy Valavala <murthy.mailme@gmail.com>
2021-08-24 18:48:24 +0000

    add story template added

--- app.py
+++ app.py
@@ -104,6 +104,11 @@ def logout():
     return redirect(url_for('login'))
 
 
+@app.route("/add_story")
+def add_story():
+    return render_template("add_story.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
